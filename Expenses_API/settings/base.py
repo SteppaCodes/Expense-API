8621f849fafce8029f18ee56052599cdd483eb88
Big Steppa <steppacodes@gmail.com>
2024-03-03 17:39:42 +0100

    chore: social auth views documentation

--- Expenses_API/settings/base.py
+++ Expenses_API/settings/base.py
@@ -1,4 +1,3 @@
-
 from datetime import timedelta
 from pathlib import Path
 from decouple import config
@@ -11,70 +10,70 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
 DJANGO_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
 THIRD_PARTY_APPS = [
-   "rest_framework",
-   "drf_spectacular",
-   "corsheaders",
-   "rest_framework_simplejwt.token_blacklist"
+    "rest_framework",
+    "drf_spectacular",
+    "corsheaders",
+    "rest_framework_simplejwt.token_blacklist",
 ]
 
 LOCAL_APPS = [
-    'apps.accounts',
-    'apps.expenses',
-    'apps.user_stats',
-    'apps.social_accounts',
+    "apps.accounts",
+    "apps.expenses",
+    "apps.user_stats",
+    "apps.social_accounts",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'Expenses_API.urls'
+ROOT_URLCONF = "Expenses_API.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'Expenses_API.wsgi.application'
+WSGI_APPLICATION = "Expenses_API.wsgi.application"
 
 
 # Database
@@ -82,28 +81,27 @@ WSGI_APPLICATION = 'Expenses_API.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('POSTGRES_DB'),
-        'USER': config('POSTGRES_USER'),
-        'PASSWORD':config('POSTGRES_PASSWORD'),
-        'HOST': config('POSTGRES_HOST'),
-        'PORT':config('POSTGRES_PORT')
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": config("POSTGRES_HOST"),
+        "PORT": config("POSTGRES_PORT"),
     }
 }
 
-AUTH_USER_MODEL = 'accounts.user'
+AUTH_USER_MODEL = "accounts.user"
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 0,
-    'NON_FIELD_ERROR_KEYS': 'error',
-    'EXCEPTION_HANDLER':'utils.exception_handler.custom_exception_handler',
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 0,
+    "NON_FIELD_ERROR_KEYS": "error",
+    "EXCEPTION_HANDLER": "utils.exception_handler.custom_exception_handler",
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    'DEFAULT_SCHEMA_CLASS':'drf_spectacular.openapi.AutoSchema'
-
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
@@ -120,16 +118,16 @@ DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -137,9 +135,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -149,26 +147,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),  
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=7),  
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=7),
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
 
 GOOGLE_CLIENT_ID = config("GOOGLE_CLIENT_ID")
-GOOGLE_CLIENT_SECRET =config("GOOGLE_CLIENT_SECRET")
+GOOGLE_CLIENT_SECRET = config("GOOGLE_CLIENT_SECRET")
 SOCIAL_AUTH_PASSWORD = config("SOCIAL_AUTH_PASSWORD")
-
-
 
 
 SPECTACULAR_SETTINGS = {
@@ -190,10 +186,8 @@ SPECTACULAR_SETTINGS = {
     ],
     "TAGS": [
         {"name": "Auth", "description": "Authentication Endpoints"},
+        {"name": "Social Auth", "description": "Social Authentication Endpoints"},
         {"name": "Expenses", "description": "Expenses CRUD endpoints"},
-    #     {"name": "Favourites", "description": "Favourites CRUD endpoints"},
-    #     {"name": "Groups", "description": "Groups CRUD endpoints"},
+        {"name": "Stats", "description": "User Stats"},
     ],
 }
-
-
--- apps/social_accounts/views.py
+++ apps/social_accounts/views.py
@@ -3,12 +3,25 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response 
+from drf_spectacular.utils import extend_schema
 
 from .serializers import GoogleSignInSerializer
+
+tags = ["Social Auth"]
 
 class GoogleSignInView(APIView):
     serialiser_class = GoogleSignInSerializer
 
+    @extend_schema(
+        tags=tags,
+        summary="Sign in with google",
+        description="This endpoint allows users to sign in or sign up with Google",
+        request=GoogleSignInSerializer,
+        responses={
+            201: GoogleSignInSerializer,
+            400: GoogleSignInSerializer,
+        }
+    )
     def post(self, request):
         serializer = self.serialiser_class(data=request.data)
         serializer.is_valid(raise_exception=True)
