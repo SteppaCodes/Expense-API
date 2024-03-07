066f65b72f31b5ae1e3a85bc772f3c61785cf5a9
Big Steppa <steppacodes@gmail.com>
2024-03-07 01:49:23 +0100

    chore: build file

--- build_files.sh
+++ build_files.sh
@@ -1,17 +1,6 @@
-# echo "BUILD START"
-# python3.9 -m pip install -r requirements.txt
-# python3.9 manage.py migrate
-# python3.9 manage.py collectstatic --noinput --clear
-# echo "BUILD END"
-
 echo "BUILD START"
-{
-  "installCommand": "npm install && python3 -m pip install -r requirements.txt && pip install urllib3<2 && python3 -m pip install -e .",
-  "buildCommand": "npm run build && python3 -m sphinx -M html docs/ _build",
-  "outputDirectory": "_build/html",
-  "github": {
-    "silent": true
-  }
-}
-
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py migrate
+python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
+
