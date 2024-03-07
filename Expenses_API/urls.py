76a34dc34cd14e0b7ada48eeff03fe97ba266a4c
Big Steppa <steppacodes@gmail.com>
2024-03-07 02:59:00 +0100

    fix: url name

--- Expenses_API/urls.py
+++ Expenses_API/urls.py
@@ -7,16 +7,13 @@ from .views import AutoRedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
-    path('api/schema', SpectacularAPIView.as_view(), name='schema'),
-    path('api/', SpectacularSwaggerView.as_view(url_name="schema")),
-
+    path("api/schema", SpectacularAPIView.as_view(), name="schema"),
+    path("api/", SpectacularSwaggerView.as_view(url_name="schema"), name="api"),
     path("api/", include("apps.accounts.urls")),
     path("api/", include("apps.expenses.urls")),
     path("api/", include("apps.user_stats.urls")),
     path("api/", include("apps.social_accounts.urls")),
-
-    path("", AutoRedirectView.as_view())
+    path("", AutoRedirectView.as_view()),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
